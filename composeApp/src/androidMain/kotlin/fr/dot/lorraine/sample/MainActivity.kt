d050f368c7710480eca847a2092e0f0f112641d9
doTTTTT <raphael.teyssandier@epitech.eu>
2025-05-29 14:58:57 +0200

    feature: Bump bunch of libs

--- composeApp/src/androidMain/kotlin/fr/dot/lorraine/sample/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/fr/dot/lorraine/sample/MainActivity.kt
@@ -6,15 +6,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
+        WindowCompat.getInsetsController(
+            window, window.decorView
+        )
+            .isAppearanceLightStatusBars = true
+
         setContent {
             App()
         }
     }
+
 }
 
 @Preview

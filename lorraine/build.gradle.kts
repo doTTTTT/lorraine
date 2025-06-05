1a503ef97b6ff739099d84b76f4fc5848160851c
doTTTTT <raphael.teyssandier@epitech.eu>
2025-06-05 09:16:37 +0200

    fix: Grdale

--- lorraine/build.gradle.kts
+++ lorraine/build.gradle.kts
@@ -98,6 +98,7 @@ room {
 }
 
 dependencies {
+    kspCommonMainMetadata(libs.androidx.room.compiler)
     add("kspAndroid", libs.androidx.room.compiler)
     add("kspIosX64", libs.androidx.room.compiler)
     add("kspIosArm64", libs.androidx.room.compiler)

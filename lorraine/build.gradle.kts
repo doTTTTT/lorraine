3beba4fbbfff47791e6e2af80fe34952568125ec
Raphael Teyssandier <rteyssandier@sephora.fr>
2025-08-02 22:09:06 +0200

    fix: Gradle ?

--- lorraine/build.gradle.kts
+++ lorraine/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
@@ -162,6 +161,6 @@ mavenPublishing {
         }
     }
 
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
     signAllPublications()
 }
